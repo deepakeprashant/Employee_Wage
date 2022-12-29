36f8740c86e3910904be8d2904604792b833b887
Prashant Deepake <deepakeprashant1@gmail.com>
2022-12-29 18:25:27 +0530

    EMP WAGE IN SWITCH CASE USE

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,15 +1,57 @@
+import java.util.Scanner;
+
 public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int PART_TIME_HOUR = 8;
     static final int FULL_TIME_HOUR = 12;
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
+    static final int ATTENDENCE =1;
+    static final int DAILY_WAGE = 2;
+    static final int PART_TIME =3;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
         EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.partTimeWageCalculate();
+        Scanner scan = new Scanner(System.in);
+        double attendence = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println("1] Employee Attendence Check");
+        System.out.println("2] Employee Daily Wage Calculate");
+        System.out.println("3] Employee Part Or Full Time Wage Calculate");
+        System.out.println("\n Choice Program");
+        int choice = scan.nextInt();
+        switch (choice){
+            case ATTENDENCE :
+                employeeWage.attendenceCheck(attendence);
+                break;
+            case DAILY_WAGE:
+                employeeWage.dailyWageCalculate(attendence);
+                break;
+            case PART_TIME:
+                employeeWage.partTimeWageCalculate();
+                break;
+            default:
+                System.out.println("WRONG CHOICE");
+        }
+    }
+    private void attendenceCheck(double attendence) {
+        if (attendence == 0) {
+            System.out.println("Employee is Absent");
+        } else {
+            System.out.println("Employee is Present");
+        }
+    }
 
+    private void dailyWageCalculate(double attendence) {
+        int empWage = 0;
+        if (attendence == 0) {
+            System.out.println("Employee is Absent");
+            System.out.println("Employee Today Wage = " + empWage);
+        } else {
+            System.out.println("Employee is Present");
+            empWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+            System.out.println("Employee Today Wage = " + empWage);
+        }
     }
 
     private void partTimeWageCalculate() {
