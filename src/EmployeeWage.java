7ff4935dfcddec25a5bc2d4e38f5d739637181c9
Prashant Deepake <deepakeprashant1@gmail.com>
2022-12-29 17:01:29 +0530

    EMP DAILY WAGE CALCULATE

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,17 +1,32 @@
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR = 8;
+    static final int FULL_DAY_HOUR = 20;
+
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
         EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.attendenceCheck();
+        double attendence = (int) Math.floor(Math.random() * 10) % 2;
+        employeeWage.attendenceCheck(attendence);
+        employeeWage.dailyWageCalculate(attendence);
+
     }
 
-    private void attendenceCheck() {
-        double attendence = (int) Math.floor(Math.random() * 10) % 2;
-        if (attendence == 0){
+    private void attendenceCheck(double attendence) {
+        if (attendence == 0) {
             System.out.println("Employee is Absent");
+        } else {
+            System.out.println("Employee is Present");
+        }
+    }
+
+    private void dailyWageCalculate(double attendence) {
+        int empWage = 0;
+        if (attendence ==0){
+            System.out.println("Employee Today Wage = "+empWage);
         }
         else {
-            System.out.println("Employee is Present");
+            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR ;
+            System.out.println("Employee Today Wage = "+empWage);
         }
     }
 }
