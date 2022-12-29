952b6345a8a77e05d6637475e968536c86639fe8
Prashant Deepake <deepakeprashant1@gmail.com>
2022-12-29 19:50:28 +0530

    EMP MONTH WAGE TILL CONDITION

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -11,6 +11,8 @@ public class EmployeeWage {
     static final int PART_TIME = 3;
     static final int MONTHLY_WAGE = 4;
     static final int TOTAL_WORKING_DAY = 20;
+    static final int WAGE_CONDITION = 5;
+    static final int TOTAL_WORKING_HOURS = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
@@ -21,6 +23,7 @@ public class EmployeeWage {
         System.out.println("2] Employee Daily Wage Calculate");
         System.out.println("3] Employee Part Or Full Time Wage Calculate");
         System.out.println("4] Employee Monthly Wage Calculate");
+        System.out.println("5] Employee Wage Till Hours Condition");
         System.out.println("\n Choice Program");
         int choice = scan.nextInt();
         switch (choice) {
@@ -35,6 +38,9 @@ public class EmployeeWage {
                 break;
             case MONTHLY_WAGE:
                 employeeWage.monthlyWageCalculate();
+                break;
+            case WAGE_CONDITION:
+                employeeWage.wageTillCondition();
                 break;
             default:
                 System.out.println("WRONG CHOICE");
@@ -85,6 +91,43 @@ public class EmployeeWage {
         int countAbsentDays = 0;
         int totalWorkingDays = 0;
         for (int day = 0; day < TOTAL_WORKING_DAY; day++) {
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empCheck) {
+                case IS_FULL_TIME:
+                    workingHour += FULL_TIME_HOUR;
+                    countFullDaysWorking++;
+                    break;
+                case IS_PART_TIME:
+                    workingHour += PART_TIME_HOUR;
+                    countHalfDayWorking++;
+                    break;
+                default:
+                    countAbsentDays++;
+            }
+        }
+        if (countAbsentDays == 20)
+            System.out.println("Employee is absent for month");
+        empWage = workingHour * WAGE_PER_HOUR;
+        totalWorkingDays = countFullDaysWorking + countHalfDayWorking;
+        System.out.println("Employee is Absent for : " + countAbsentDays + " days.");
+        System.out.println("Employee is Half day present for : " + countHalfDayWorking + " days.");
+        System.out.println("Employee is full day present for : " + countFullDaysWorking + " days.");
+        System.out.println("Employee is present for " + totalWorkingDays);
+        System.out.println(" days in a month and wage is : " + empWage + " Rs");
+    }
+
+    private void wageTillCondition() {
+        int workingHour = 0;
+        int empWage = 0;
+        int countFullDaysWorking = 0;
+        int countHalfDayWorking = 0;
+        int countAbsentDays = 0;
+        int totalWorkingDays = 0;
+        for (int day = 0; day < TOTAL_WORKING_DAY; day++) {
+            if (workingHour > TOTAL_WORKING_HOURS) {
+                workingHour = TOTAL_WORKING_HOURS;
+                break;
+            }
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
                 case IS_FULL_TIME:
