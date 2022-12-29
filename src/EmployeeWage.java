af33daf3dbb868132a327b7e0fa5547f2474c3f6
Prashant Deepake <deepakeprashant1@gmail.com>
2022-12-29 18:06:51 +0530

    EMP PART OR FULL TIME WAGE

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,32 +1,30 @@
 public class EmployeeWage {
-    static final int WAGE_PER_HOUR = 8;
-    static final int FULL_DAY_HOUR = 20;
+    static final int WAGE_PER_HOUR = 20;
+    static final int PART_TIME_HOUR = 8;
+    static final int FULL_TIME_HOUR = 12;
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
         EmployeeWage employeeWage = new EmployeeWage();
-        double attendence = (int) Math.floor(Math.random() * 10) % 2;
-        employeeWage.attendenceCheck(attendence);
-        employeeWage.dailyWageCalculate(attendence);
+        employeeWage.partTimeWageCalculate();
 
     }
 
-    private void attendenceCheck(double attendence) {
-        if (attendence == 0) {
-            System.out.println("Employee is Absent");
-        } else {
-            System.out.println("Employee is Present");
-        }
-    }
-
-    private void dailyWageCalculate(double attendence) {
+    private void partTimeWageCalculate() {
         int empWage = 0;
-        if (attendence ==0){
-            System.out.println("Employee Today Wage = "+empWage);
-        }
-        else {
-            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR ;
-            System.out.println("Employee Today Wage = "+empWage);
+        double attendence = (int) Math.floor(Math.random() * 10) % 3;
+        if (attendence == IS_FULL_TIME) {
+            System.out.println("Employee is Present Full Time");
+            empWage = WAGE_PER_HOUR * FULL_TIME_HOUR ;
+            System.out.println("Full Time Employee Wage : "+empWage);
+        } else if (attendence==IS_PART_TIME) {
+            System.out.println("Employee is Present Part Time");
+            empWage = WAGE_PER_HOUR * PART_TIME_HOUR ;
+            System.out.println("Part Time Employee Wage : "+empWage);
+        }else {
+            System.out.println("Employee is Absent");
         }
     }
 }
