201862afdc199569ee22e4d4e32d0c285551e70a
Prashant Deepake <deepakeprashant1@gmail.com>
2022-12-29 16:38:56 +0530

    EMPLOYEE WAGE ATTENDENCE CHECK

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,17 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.attendenceCheck();
+    }
+
+    private void attendenceCheck() {
+        double attendence = (int) Math.floor(Math.random() * 10) % 2;
+        if (attendence == 0){
+            System.out.println("Employee is Absent");
+        }
+        else {
+            System.out.println("Employee is Present");
+        }
     }
 }
